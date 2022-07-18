f6b679fe43bc349a77ddf866cd8fc2aaa14f524c
Abrarhusamfahim <fahimjahin321@gmail.com>
2022-07-18 21:02:42 +0600

    first web socket

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+
+const app = express();
+const httpServer = http.createServer(app)
+
+const ws = require("ws");
+const wss = new ws.Server({server: httpServer});
+
+app.get("/", (req,res)=>{
+    res.sendFile(`${__dirname}/index.html`)
+})
+
+wss.on("connection", (socket)=>{
+    console.log("Client connected");
+    socket.on("message", (data)=>{
+        const msg = data.toString();
+        wss.clients.forEach(client => {
+            if(client !== socket){
+                client.send(msg)
+            }
+           
+        })
+    });
+    socket.on("close", ()=>{
+        console.log("User disconnected");
+    })
+});
+
+httpServer.listen(3030, ()=>{
+    console.log("Server is running @3030");
+})
